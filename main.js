07dcbf193e6e0a0c1cb5c65ae38cbe6967b1eae5
ferucci <development.first1@gmail.com>
2022-03-29 00:39:43 +0300

    confused

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 "use strict";
+
+let num = 266219;
+const array = Array.from(String(num));
+let item = 1;
+
+for (let i = 0; i < array.length; i++) {
+  item = item * array[i];
+}
+console.log(item);
+
+let expo = item ** 3;
+let expoArr = Array.from(String(expo));
+
+console.log(expoArr[0], expoArr[1]);
