818cf082e99745270254b1bdc8ea7f65c09db1d3
ferucci <development.first1@gmail.com>
2022-04-03 17:57:27 +0300

    implemented lesson03-hard

--- main.js
+++ main.js
@@ -1,1 +1,56 @@
 "use strict";
+let lang;
+
+let date = new Date();
+let options = {
+  year: 'numeric',
+  month: 'long',
+  weekday: 'long',
+  day: '2-digit'
+};
+
+let langArr = [
+  ["ru", 'en']
+];
+
+if (lang === 'ru') {
+  console.log(date.toLocaleString('ru', options));
+} else if (lang === 'en') {
+  console.log(date.toLocaleString('en-GB', options));
+} else {
+  console.log(date.toLocaleString('zh-Hans-CN', options));
+}
+
+switch (lang) {
+  case 'ru':
+    console.log(date.toLocaleString('ru', options));
+    break;
+  case 'en':
+    console.log(date.toLocaleString('en-GB', options));
+    break;
+  default:
+    console.log('Тогда на китайском', date.toLocaleString('zh-Hans-CN', options));
+}
+
+
+
+for (let i = 0; i < langArr.length; i++) {
+
+  for (let i = 0; i < langArr.length; i++) {
+    (langArr[0][0]) = console.log(date.toLocaleString('ru', options));
+    (langArr[0][1]) = console.log(date.toLocaleString('en-GB', options));
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
